192907a584ffcd6d03793e4ff37d302253413f19
Yusuprozimemet <yusuf.rozimemet@gmail.com>
2026-06-08 23:47:45 +0200

    database is connected

---
+++ postify/src/main/java/net/hackyourfuture/backend/week6/postify/DatabaseConnectionCheck.java
@@ -0,0 +1,21 @@
+package net.hackyourfuture.backend.week6.postify;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseConnectionCheck {
+     private final JdbcTemplate jdbcTemplate;
+
+    public DatabaseConnectionCheck(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void checkConnection() {
+        String result = this.jdbcTemplate.queryForObject("SELECT 'connected'", String.class);
+        System.out.println("Database status: " + result);
+    }
+}
